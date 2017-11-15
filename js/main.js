18144ac9b3665079885851533908e73aca97a079
alina3agit <antonova.alina@gmail.com>
2017-11-16 00:30:30 +0100

    Navigation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function($) {
 	
-	/* Call plugins here ...*/
+	var pull = $("#navigation__toggle");
+	var nav = $("#navigation__items");
+
+	$(pull).on('click', function(e) {
+		e.preventDefault();		
+		$(nav).slideToggle();
+		$(this).toggleClass('navigation__toggle-button--active');
+	});
+
+	$(window).resize(function() {
+		var w = $(window).width();
+		if(w > 992) {
+			nav.removeAttr('style');
+		}
+	});
 
 });
