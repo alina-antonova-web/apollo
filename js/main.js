8df8f19ae2e32d0317d84cf7f87b34f3385f9460
alina3agit <antonova.alina@gmail.com>
2017-11-20 12:21:19 +0100

    Slide2id

--- js/main.js
+++ js/main.js
@@ -15,11 +15,40 @@ $(document).ready(function($) {
 		var w = $(window).width();
 		if(w > 992) {
 			nav.removeAttr('style');
+			pull.removeClass('navigation__toggle-button--active');
+		} else {
+
+	    }
+	});
+
+	// Скрываем меню при клике на него на смартфоне и планцете
+	// По клику на ссылку в меню запускаем ф-ю fnstart();
+	$('nav.navigation a').on("click", function(){
+		fnstart();
+	});
+
+	// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
+	// тогда убираем класс модификатор --active у кнопки pull
+	// и сворачиваем/скрываем меню 
+	function fnstart(){	
+		if ( pull.hasClass("navigation__toggle-button--active")  ) {
+   			pull.toggleClass('navigation__toggle-button--active');
+			nav.slideToggle();
 		}
+	};
+
+
+
+	
+
+	//slide2id
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
 	});
 
 
-	//Slider
+
+	//  Slider
 	$("#top_slider").owlCarousel({
 	  	items: 1,
 	  	nav: true,
@@ -28,5 +57,8 @@ $(document).ready(function($) {
 	  	theme: "top-slider-theme",
 
 	});
+
+
+
 
 });
